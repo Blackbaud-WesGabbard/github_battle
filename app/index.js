ad3e095678a8b96b29e736988d5f770369693b3e
Wes Gabbard <wes.gabbard@blackbaud.com>
2017-12-30 15:55:43 -0600

    add prop types

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
 
@@ -13,10 +14,36 @@ class App extends React.Component {
   }
 }
 
+class Badge extends React.Component {
+  render(){
+    return (
+      <div>
+        <img 
+          src={this.props.img}
+          alt='Avatar'
+          style={{width:100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
+      </div>
+    )
+  }
+}
+
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+}
 
 
 ReactDOM.render(
-  <App />,
+  /*<App />,*/
+  <Badge
+    name='Wes Gabbard'
+    username='wgabbard'
+    img = 'https://avatars0.githubusercontent.com/u/29333430?v3&s=460'
+  />,
   document.getElementById('app')
 );
 
