2a04ff59920bca52e765b412e6d2b861865cbe02
Wes Gabbard <wes.gabbard@blackbaud.com>
2018-01-03 11:42:54 -0600

    refactor selectLanguage into stateless function

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,29 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className="languages">
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: 'red'}: null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+            {lang}
+          </li>
+        )
+      }, this)}
+
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component {
   constructor (props) {
@@ -19,23 +44,11 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-    console.log('Uphere', this);
     return (
-      <ul className="languages">
-        {languages.map(function (lang) {
-          console.log('down',this);
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? {color: 'red'}: null}
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}>
-              {lang}
-            </li>
-          )
-        }, this)}
-
-      </ul>
+      <SelectLanguage 
+        selectedLanguage={this.state.selectedLanguage}
+        onSelect={this.updateLanguage}
+      />
     )
   }
 }
