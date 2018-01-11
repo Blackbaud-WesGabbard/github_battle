165c1d74646cc301f74aeba141910cfce2b834ce
Wes Gabbard <wes.gabbard@blackbaud.com>
2018-01-11 12:55:31 -0600

    add results

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,36 +1,8 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
 
-function PlayerPreview(props) {
-  return (
-    <div>
-      <div className='column'>
-        <img 
-          className='avatar'
-          src={props.avatar}
-          alt={props.username}
-        />
-        <h2 className="username">
-          @{props.username}
-        </h2>
-        <button 
-          className="reset"
-          onClick={props.onReset.bind(null, props.id)}
-        >
-          Reset
-        </button>
-      </div>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props);
@@ -141,7 +113,7 @@ class Battle extends React.Component {
             <PlayerInput
               id="playerOne"
               label="Player One"
-              onSubmit={this.handleSubmit} 
+              onSubmit={this.handleSubmit}
             />
           }
 
@@ -149,25 +121,35 @@ class Battle extends React.Component {
             <PlayerPreview
               avatar={playerOneImage}
               username={playerOneName}
-              onReset={this.handleReset}
-              id="playerOne"
-            />
+            >
+              <button
+                className="reset"
+                onClick={this.handleReset.bind(null, 'playerOne')}
+              >
+                Reset
+              </button>
+            </PlayerPreview>
           }
 
           {playerTwoImage !== null &&
             <PlayerPreview
               avatar={playerTwoImage}
               username={playerTwoName}
-              onReset={this.handleReset}
-              id="playerTwo"
-            />
+            >
+              <button
+                className="reset"
+                onClick={this.handleReset.bind(null, 'playerTwo')}
+              >
+                Reset
+              </button>
+            </PlayerPreview>
           }
 
           {!playerTwoName &&
             <PlayerInput
               id="playerTwo"
               label="Player Two"
-              onSubmit={this.handleSubmit} 
+              onSubmit={this.handleSubmit}
             />
           }
 
@@ -183,7 +165,7 @@ class Battle extends React.Component {
               >
               BATTLE!!!
             </Link>
-          }  
+          }
       </div>
     )
   }
--- app/utils/api.js
+++ app/utils/api.js
@@ -8,16 +8,18 @@ function getProfile(username) {
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + 'repos&per_page=100')
+  return axios.get('https://api.github.com/users/' + username + '?repos&per_page=100')
     .then(function (user){
       return user.data;
     });
 }
 
 function getStarCount (repos) {
-  return repos.data.reduce(function (count, repo){
-    return count + repo.stargazers_count;
-  }, 0)
+  return repos.following;
+  /* data no longer strcutred like this, no statrgazer count so swithing to just counting follow
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count
+  }, 0);*/
 }
 
 function calculateScore (profile, repos) {
@@ -56,9 +58,10 @@ function sortPlayers (players) {
 
 module.exports = {
   battle: function (players) {
+    console.log('enter battle');
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
-      .catch(handleError)
+      .catch(handleError);
   },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
